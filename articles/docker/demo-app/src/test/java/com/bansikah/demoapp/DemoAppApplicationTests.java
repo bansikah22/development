2d4c49f6600fd0d143b874645c81cf20dd4cd06c
bansikah22 <tandapnoelbansikah@gmail.com>
2025-01-22 13:30:50 +0100

    docker article

---
+++ articles/docker/demo-app/src/test/java/com/bansikah/demoapp/DemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bansikah.demoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
