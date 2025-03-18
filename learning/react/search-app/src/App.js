4f5a96fdd4daba9d6f8f0ba186233a7701c9fd19
bansikah22 <tandapnoelbansikah@gmail.com>
2025-03-18 17:52:20 +0100

    feat: react search bar

---
+++ learning/react/search-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Search from './Search';
+
+function App() {
+  return (
+    <>
+    <Search />
+    </>
+  );
+}
+
+export default App;
---
+++ learning/react/search-app/src/data.js
@@ -0,0 +1,56 @@
+import img1 from './img/img1.png'
+
+const data = {
+     cardData: [
+        {
+            id: 1,
+            img: img1,
+            title: 'blog 1',
+            desc: 'desc 1'
+        },
+        {
+            id: 2,
+            img: img1,
+            title: 'blog 2',
+            desc: 'desc 2'
+        },
+        {
+            id: 3,
+            img: img1,
+            title: 'blog 3',
+            desc: 'desc 3'
+        },
+        {
+            id: 4,
+            img: img1,
+            title: 'blog 4',
+            desc: 'desc 4'
+        },
+        {
+            id: 5,
+            img: img1,
+            title: 'blog 5',
+            desc: 'desc 5'
+        },
+        {
+            id: 6,
+            img: img1,
+            title: 'blog 6',
+            desc: 'desc 6'
+        },
+        {
+            id: 7,
+            img: img1,
+            title: 'blog 7',
+            desc: 'desc 7'
+        },
+        {
+            id: 8,
+            img: img1,
+            title: 'blog 8',
+            desc: 'desc 8'
+        }
+     ]
+}
+
+export default data;
