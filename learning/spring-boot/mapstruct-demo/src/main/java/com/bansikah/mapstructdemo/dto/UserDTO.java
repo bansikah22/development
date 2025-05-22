a817c333c345292a987561ec17a34f3f651623a4
nta <NoelBansikah.Tandap@adorsys.com>
2025-05-22 17:54:25 +0100

    fix: add getters and setters manually

--- learning/spring-boot/mapstruct-demo/src/main/java/com/bansikah/mapstructdemo/dto/UserDTO.java
+++ learning/spring-boot/mapstruct-demo/src/main/java/com/bansikah/mapstructdemo/dto/UserDTO.java
@@ -2,12 +2,21 @@ package com.bansikah.mapstructdemo.dto;
 
 import lombok.*;
 
-@Data
-@NoArgsConstructor
+
 @AllArgsConstructor
-@Builder
 public class UserDTO {
     private Long id;
     private String name;
     private String email;
+
+    public UserDTO() {}
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 }
